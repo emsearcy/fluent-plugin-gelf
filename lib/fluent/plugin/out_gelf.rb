85fa05417be044b732dc67bb904f050522ed55c7
Alex Yamauchi <oss@hotschedules.com>
2017-03-13 19:54:32 +0000

    Fixing output plugin block indentation.

--- lib/fluent/plugin/out_gelf.rb
+++ lib/fluent/plugin/out_gelf.rb
@@ -1,84 +1,84 @@
 module Fluent
 
-class GELFOutput < BufferedOutput
+  class GELFOutput < BufferedOutput
 
-  Plugin.register_output("gelf", self)
+    Plugin.register_output("gelf", self)
 
-  require 'fluent/gelf_util'
-  include GelfUtil
+    require 'fluent/gelf_util'
+    include GelfUtil
 
-  config_param :use_record_host, :bool, :default => false
-  config_param :add_msec_time, :bool, :default => false
-  config_param :host, :string, :default => nil
-  config_param :port, :integer, :default => 12201
-  config_param :protocol, :string, :default => 'udp'
+    config_param :use_record_host, :bool, :default => false
+    config_param :add_msec_time, :bool, :default => false
+    config_param :host, :string, :default => nil
+    config_param :port, :integer, :default => 12201
+    config_param :protocol, :string, :default => 'udp'
 
-  def initialize
-    super
-    require "gelf"
-  end
-
-  def configure(conf)
-    super(conf)
-
-    # a destination hostname or IP address must be provided
-    raise ConfigError, "'host' parameter (hostname or address of Graylog2 server) is required" unless conf.has_key?('host')
-
-    # choose protocol to pass to gelf-rb Notifier constructor
-    # (@protocol is used instead of conf['protocol'] to leverage config_param default)
-    if @protocol == 'udp' then @proto = GELF::Protocol::UDP
-    elsif @protocol == 'tcp' then @proto = GELF::Protocol::TCP
-    else raise ConfigError, "'protocol' parameter should be either 'udp' (default) or 'tcp'"
-    end
-  end
-
-  def start
-    super
-
-    @conn = GELF::Notifier.new(@host, @port, 'WAN', {:facility => 'fluentd', :protocol => @proto})
-
-    # Errors are not coming from Ruby so we use direct mapping
-    @conn.level_mapping = 'direct'
-    # file and line from Ruby are in this class, not relevant
-    @conn.collect_file_and_line = false
-  end
-
-  def shutdown
-    super
-  end
-
-  def format(tag, time, record)
-    if defined? Fluent::EventTime and time.is_a? Fluent::EventTime then
-      timestamp = time.sec + (time.nsec.to_f/1000000000).round(3)
-    else
-      timestamp = time
+    def initialize
+      super
+      require "gelf"
     end
 
-    make_gelfentry(
-      tag,time,record,
-      {
-        :use_record_host => @use_record_host,
-        :add_msec_time => @add_msec_time
-      }
-    ).to_msgpack
+    def configure(conf)
+      super(conf)
 
-  end
+      # a destination hostname or IP address must be provided
+      raise ConfigError, "'host' parameter (hostname or address of Graylog2 server) is required" unless conf.has_key?('host')
 
-  def write(chunk)
-    chunk.msgpack_each do |data|
-      @conn.notify!(data)
+      # choose protocol to pass to gelf-rb Notifier constructor
+      # (@protocol is used instead of conf['protocol'] to leverage config_param default)
+      if @protocol == 'udp' then @proto = GELF::Protocol::UDP
+      elsif @protocol == 'tcp' then @proto = GELF::Protocol::TCP
+      else raise ConfigError, "'protocol' parameter should be either 'udp' (default) or 'tcp'"
+      end
     end
-  end
 
-  def formatted_to_msgpack_binary
-    true
-  end
+    def start
+      super
 
-  def formatted_to_msgpack_binary
-    true
-  end
+      @conn = GELF::Notifier.new(@host, @port, 'WAN', {:facility => 'fluentd', :protocol => @proto})
 
-end
+      # Errors are not coming from Ruby so we use direct mapping
+      @conn.level_mapping = 'direct'
+      # file and line from Ruby are in this class, not relevant
+      @conn.collect_file_and_line = false
+    end
+
+    def shutdown
+      super
+    end
+
+    def format(tag, time, record)
+      if defined? Fluent::EventTime and time.is_a? Fluent::EventTime then
+        timestamp = time.sec + (time.nsec.to_f/1000000000).round(3)
+      else
+        timestamp = time
+      end
+
+      make_gelfentry(
+        tag,time,record,
+        {
+          :use_record_host => @use_record_host,
+          :add_msec_time => @add_msec_time
+        }
+      ).to_msgpack
+
+    end
+
+    def write(chunk)
+      chunk.msgpack_each do |data|
+        @conn.notify!(data)
+      end
+    end
+
+    def formatted_to_msgpack_binary
+      true
+    end
+
+    def formatted_to_msgpack_binary
+      true
+    end
+
+  end
 
 end
 
